5b08644418e96ab9fb155a13eb1954778ad66c12
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-03-17 17:09:31 +0000

    sol

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,8 @@ import  '../styles/App.css';
 
 function App()
 {
-    return (
-         <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-          )
+    return <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+          
 }
 
 export default App;
